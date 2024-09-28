9b6d5f96bbe68b4720bfe025c9236b45101e51e3
mikaquiapos19 <mikaquiapos19@gmail.com>
2024-09-28 23:57:01 +0100

    Completed gradebook.py - adding, removing, and concatenation

--- gradebook.py
+++ gradebook.py
@@ -6,3 +6,38 @@
 # Recording data from last semester's gradebook
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 
+# Creating a list of the current semester's subjects
+subjects = ["physics", "calculus", "poetry", "history"]
+
+# Creating a list called grades and filling it with current scores
+grades = [98, 97, 85, 88]
+
+# Manually (without any methods) creating a 2-D list to combine subjects and grades
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+# Displaying the gradebook in its current state
+print(gradebook)
+
+# Adding grade for computer science class with a perfect score of 100
+gradebook.append(["computer science", 100])
+# print(gradebook)
+
+# Adding grade for visual arts with a score of 93
+gradebook.append(["visual arts", 93])
+# print(gradebook)
+
+# Modifying the gradebook - rewarding an extra 5 points for visual arts
+gradebook[5][1] = 98
+
+# Student decides to switch from a numerical grade value to a Pass/Fail option in poetry class
+# Removing the grade value in gradebook for poetry
+gradebook[2].remove(85)
+# print(gradebook)
+
+# Adding "Pass" value for poetry
+gradebook[2].append("Pass")
+# print(gradebook)
+
+# Storing last semester and current semester gradebook into one full gradebook
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
