3d811c22e6f8d7411d2cd2e33cfe2056a71693ad
mikaquiapos19 <mikaquiapos19@gmail.com>
2024-09-28 23:23:21 +0100

    Creating gradebook.py (I tried to do this previously... in the incorrect folder)

---
+++ gradebook.py
@@ -0,0 +1,8 @@
+# Program Description:  This program creates a gradebook for students to organise subjects and grades
+#                       This program is for practising lists
+# Author: Mika Quiapos
+# Date: September 28/09/24
+
+# Recording data from last semester's gradebook
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
