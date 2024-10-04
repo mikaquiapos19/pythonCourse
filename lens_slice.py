470e5eeb458ccb93ca3c3ac2c190c266ad0d725b
mikaquiapos19 <mikaquiapos19@gmail.com>
2024-10-04 23:30:16 +0100

    Creating and starting the file lens_slice.py + minor additions to ignore certain spellings in settings

---
+++ lens_slice.py
@@ -0,0 +1,7 @@
+# Program Description:  This program organises sales data for a pizza joint called "Len's Slice".
+#                       This program is for practicing lists
+# Author: Mika Quiapos
+# Date: 04/10/24
+
+# Keeping track of the toppings available using a list
+toppings = ["pepperoni", "pineapple", "cheese", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
